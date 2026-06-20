939024c4e939ef1be193e051f3b3906696150a32
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-06-20 01:37:29 -0300

    SW v8 - limpia todo el cache anterior

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = 'finanzas-v5';
+const CACHE = 'finanzas-v8';
 
 self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', e => {
     e.waitUntil(
         caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+            Promise.all(keys.map(k => caches.delete(k)))
         )
     );
     self.clients.claim();
