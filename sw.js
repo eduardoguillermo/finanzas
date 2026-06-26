f1998a65dd597e600a857fd4b1a75e162df309bc
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-06-26 12:30:54 -0300

    sw.js: v24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finanzas-v23';
+const CACHE = 'finanzas-v24';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting();});
