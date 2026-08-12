f506a4cae4cb9e7bfe63d69cc18b6b901a5782c0
eduardoguillermo <70851744+eduardoguillermo@users.noreply.github.com>
2026-08-12 12:48:10 -0300

    Movimientos bancarios: zebra striping en tabla de detalle

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'finanzas-v3.7.91';
+const CACHE = 'finanzas-v3.7.92';
 const ASSETS = ['./control_financiero_v3.html', './app.js', './main.js', './manifest.json', './xlsx.full.min.js', './instructivo.html'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => Promise.all(ASSETS.map(a => fetch(a + '?v=' + Date.now(), {cache:'no-store'}).then(r => { if(r.ok) c.put(a, r); }).catch(()=>{}))))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
